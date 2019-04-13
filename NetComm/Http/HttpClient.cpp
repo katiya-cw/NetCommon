cda198e7fd873511a8a165e533356cb04470889a
liuzebo <liuxiao.bobo1988@gmail.com>
2019-04-13 19:27:45 +0800

    http: implement sync request

--- NetComm/Http/HttpClient.cpp
+++ NetComm/Http/HttpClient.cpp
@@ -1,6 +1,6 @@
 #include "HttpClient.h"
 #include <evpp/httpc/conn.h>
-//#include <folly/futures/Future.h>
+#include <future>
 
 CHttpClient::CHttpClient() {
 	m_loop.Start(true);
@@ -10,22 +10,21 @@ CHttpClient::~CHttpClient() {
 	m_loop.Stop(true);
 }
 
-/*
+
 void CHttpClient::HandleHTTPResponse(const std::shared_ptr<evpp::httpc::Response>& response, 
-	evpp::httpc::GetRequest* request, folly::Promise<std::string>* resPromise) {
+	evpp::httpc::GetRequest* request, std::promise<std::string>& resPromise) {
 	delete request; // The request MUST BE deleted in EventLoop thread.
-	resPromise->setValue(response->body().ToString());
+	resPromise.set_value(response->body().ToString());
 }
-*/
+
 std::string CHttpClient::request(std::string url) {
 	evpp::httpc::GetRequest* r = new evpp::httpc::GetRequest(m_loop.loop(), url.c_str(), evpp::Duration(5.0));
-	/*
-	folly::Promise<std::string> resPromise;
-	folly::Future<std::string> resFuture = resPromise.getFuture();
-	r->Execute(std::bind(&CHttpClient::HandleHTTPResponse, std::placeholders::_1, r, &resPromise));
-	std::string response = std::move(resFuture).get();
-	*/
-	std::string response = "";
+	
+	std::promise<std::string> resPromise;
+	std::future<std::string> resFuture = resPromise.get_future();
+	r->Execute(std::bind(&CHttpClient::HandleHTTPResponse, std::placeholders::_1, r, std::ref(resPromise)));
+	std::string response = resFuture.get();
+	
 	return response;
 }
 
--- NetComm/Http/HttpClient.h
+++ NetComm/Http/HttpClient.h
@@ -6,7 +6,7 @@
 #include <evpp/event_loop_thread.h>
 #include <evpp/httpc/request.h>
 #include <evpp/httpc/response.h>
-
+#include <future>
 
 class CHttpClient {
 public:
@@ -17,10 +17,8 @@ public:
 	std::string request(std::string url);
 
 public:
-	/*
 	static void HandleHTTPResponse(const std::shared_ptr<evpp::httpc::Response>& response, evpp::httpc::GetRequest* request, 
-		folly::Promise<std::string>* resPromise);
-	*/
+		std::promise<std::string>& resPromise);
 
 private:
   evpp::EventLoopThread m_loop;	
